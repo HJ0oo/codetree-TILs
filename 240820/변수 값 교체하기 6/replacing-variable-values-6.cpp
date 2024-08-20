8fecf8a91fdd36aeae10563d6d6bf0890483f1ac
HJ0oo <ihjkong@naver.com>
2024-08-20 20:16:26 +0900

    Title: 변수 값 교체하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240820/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a = 2;
+    int b = 5;
+    int temp;
+
+
+    temp = a ;
+    a = b ;
+    b = temp ;
+
+
+    cout << a << "\n" << b ;
+
+    
+    
+
+
+
+
+    return 0;
+}
