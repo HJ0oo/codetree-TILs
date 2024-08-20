bbe023adbcbb51dc4a387e52ccac2dd7d7d2a970
HJ0oo <ihjkong@naver.com>
2024-08-20 19:28:59 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240820/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+    int weight = 13 ;
+    double gravity = 0.165 ;
+    cout << weight<<" * "<<gravity<<" = "<< weight * gravity;
+    
+    return 0;
+}
