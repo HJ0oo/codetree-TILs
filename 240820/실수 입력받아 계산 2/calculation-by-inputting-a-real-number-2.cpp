b4e1d3cc1a025ef9249efe5e915303491644675e
HJ0oo <ihjkong@naver.com>
2024-08-20 21:02:45 +0900

    Title: 실수 입력받아 계산 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240820/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+    cout.precision(2);
+
+    double a ;
+    cin >> a ;
+    cout << a + 1.5 ;
+
+
+
+
+
+    return 0;
+}
