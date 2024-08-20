4caae5a505cf4f1c42241abcec13affb87aad710
HJ0oo <ihjkong@naver.com>
2024-08-20 20:57:27 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240820/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+    cout.precision(2);
+    double n ;
+    cin >> n ;
+    cout << n ;
+
+
+
+
+
+
+
+    return 0;
+}
