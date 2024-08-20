1c479ea70406b4d1bf3cfbc38f4814c5c18fd4c0
HJ0oo <ihjkong@naver.com>
2024-08-20 20:21:42 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240820/데이터 교환/exchange-data.cpp
+++ 240820/데이터 교환/exchange-data.cpp
@@ -4,15 +4,11 @@ using namespace std;
 int main() {
     
     int a = 5 , b = 6 , c = 7 ;
-    int temp1 , temp2, temp3 ;
-
-    temp1 = a ; 
-    temp2 = b ;
-    temp3 = c ;
-
-    b = temp1 ;
-    c = temp2 ;
-    a = temp3 ;
+    
+    int temp = a;
+	a = c;
+    c = b;
+	b = temp;
 
     cout << a << "\n" << b << "\n" << c ;
 
