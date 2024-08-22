779eeacb0a179ed1b38e568f7243219431ea4374
HJ0oo <ihjkong@naver.com>
2024-08-22 15:37:25 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240822/살 수 있는 물건 2/things-able-to-buy-2.cpp
+++ 240822/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -9,10 +9,10 @@ int main() {
     if (n >= 3000){
         cout << "book";
     }
-    else if (n >= 3000){
+    else if (n >= 1000){
         cout << "mask";
     }
-    else if (n >= 3000){
+    else if (n >= 500){
         cout << "pen";
     }
     else {
