549234489842a82bce81b893e2bf11bd2954d602
HJ0oo <ihjkong@naver.com>
2024-10-23 11:35:27 +0900

    Title: 가장 많은 데이터, Time: 215ms, Memory: 47MB, Status: Passed - CodeTree

--- 241023/가장 많은 데이터/most-frequent-data.cpp
+++ 241023/가장 많은 데이터/most-frequent-data.cpp
@@ -16,6 +16,23 @@ int main() {
     for (int i=0;i<n;i++){
         cin >> arr[i];
         my_map[arr[i]] += 1; 
+        /*
+        C++에서 std::map이나 std::unordered_map과 같은 연관 컨테이너들이
+        기본적으로 default-initialization을 지원하기 때문에
+            // 처음 나온 문자열이라면 1을 직접 적어줘야 합니다.
+            if(freq.find(words[i]) == freq.end())
+                freq[words[i]] = 1;
+            // 이미 나와있던 문자열이라면 1을 더해줍니다.
+            else
+                freq[words[i]]++;
+        이런 식으로 안해도 돌아감.
+
+        자료형에 따라 자동 초기화되는 기본값:
+            정수형(int): 0
+            실수형(float, double): 0.0
+            bool: false
+            포인터형: nullptr
+        */
         max_num = max(max_num,my_map[arr[i]]);
     }
 
