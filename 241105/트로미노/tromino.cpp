8c109e056aef8b7c0c65257bea59a720d9071349
HJ0oo <ihjkong@naver.com>
2024-11-05 14:55:59 +0900

    Title: 트로미노, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 241105/트로미노/tromino.cpp
+++ 241105/트로미노/tromino.cpp
@@ -8,7 +8,7 @@ int max_ans = -1; // 자연수만 들어있으므로 -1로 설정
 
 int cal_case_1(int s_i, int s_j) {
     // ㄱ자 모양의 최댓값 = 2*2에서 최소를 빼기
-    if ( (s_i + 1 < n ) && ( s_j + 1 < n )) {
+    if ( (s_i + 1 < n ) && ( s_j + 1 < m )) {
         return arr[s_i][s_j] + arr[s_i+1][s_j] + arr[s_i][s_j+1]+ arr[s_i+1][s_j+1] - min({arr[s_i][s_j], arr[s_i+1][s_j], arr[s_i][s_j+1], arr[s_i+1][s_j+1]});
     }
     return 0;
@@ -24,8 +24,8 @@ int cal_case_2(int s_i, int s_j) {
 }
 
 int cal_case_3(int s_i, int s_j) {
-    // ㅣ자 모양의 최댓값 = 가로 세로 모두 고려해야함
-    if (s_j + 2 < n ) {
+    // ㅡ자 모양의 최댓값
+    if (s_j + 2 < m ) {
         return arr[s_i][s_j] + arr[s_i][s_j+1] + arr[s_i][s_j+2];
     }
     return 0;
@@ -44,12 +44,13 @@ int main() {
 
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++){
-            // cout << " i와 j가" << i << " " << j <<endl;
             max_ans = max(cal_case_1(i,j), max_ans);
-            // cout << cal_case_1(i,j) << endl;
-            // cout << cal_case_2(i,j) << endl;
             max_ans = max(cal_case_2(i,j), max_ans);
             max_ans = max(cal_case_3(i,j), max_ans);
+            // cout << "i와 j가" << i << " " << j <<endl;
+            // cout << cal_case_1(i,j) << " ㄱ" << endl;
+            // cout << cal_case_2(i,j) << " | " << endl;
+            // cout << cal_case_3(i,j) << " ㅡ " << endl;
         }
     }
 
