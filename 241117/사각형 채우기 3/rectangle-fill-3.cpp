0cc51a634ca420e9241ac0de453b11c38e8f4cc1
HJ0oo <ihjkong@naver.com>
2024-11-17 01:08:15 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241117/사각형 채우기 3/rectangle-fill-3.cpp
+++ 241117/사각형 채우기 3/rectangle-fill-3.cpp
@@ -12,8 +12,9 @@ int main() {
     cin >> n;
     arr[1] = 2;
     arr[2] = 7;
-    for (int i=3; i<n+1; i++) {
-        arr[i] = arr[i-1] * 2 + arr[i-2] * 3 + 2;
+    arr[3] = 22;
+    for (int i=4; i<n+1; i++) {
+        arr[i] = arr[i-1] * 2 + arr[i-2] * 3 + arr[i-3] * 2 + 2;
         arr[i] %= 1000000007;   
     }
     cout << arr[n];
