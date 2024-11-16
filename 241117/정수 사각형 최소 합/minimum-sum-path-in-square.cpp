b6f44bb80e0b8173c971d7803eaba6264dc9fb32
HJ0oo <ihjkong@naver.com>
2024-11-17 02:15:52 +0900

    Title: 정수 사각형 최소 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241117/정수 사각형 최소 합/minimum-sum-path-in-square.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[101][101] = {0};
+int ans[101][101] = {0};
+int n;
+
+int main() {
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    // (0,n-1) > (n-1,0) 왼쪽 혹은 밑으로만 이동
+    for(int i = 0; i <= n-1; i++){
+        for(int j = n-1; j >= 0; j--){
+            if (i == 0) {
+                ans[i][j] = arr[i][j] + ans[i][j+1];
+            }
+            if (j == n-1) {
+                ans[i][j] = arr[i][j] + ans[i-1][j];
+            }
+            if (( i != 0) && (j != n-1)){
+                ans[i][j] = min(ans[i][j+1],ans[i-1][j]) + arr[i][j];
+            }            
+            // cout << i << " " << j << " " << ans[i][j] << endl;
+        }
+    }
+    cout << ans[n-1][0];
+    return 0;
+}
