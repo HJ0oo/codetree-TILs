e74ddf47520126e58354df4005b29ba5c3143796
HJ0oo <ihjkong@naver.com>
2024-11-17 16:57:41 +0900

    Title: 최대 증가 부분 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241117/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <numeric> 
+using namespace std;
+
+int n;
+int arr[1001] = {0};
+int dp[10001] = {0};
+
+int main() {
+    cin >> n;
+    for (int i=1;i<n+1;i++){
+        cin >> arr[i];
+    }
+
+    for (int i=1; i<n+1; i++){
+        dp[arr[i]] = reduce(dp,dp + arr[i]+1, 0, max<int>) + 1;
+        // cout <<  dp[arr[i]]  << " , i는 " << arr[i] << endl;
+    }
+
+    cout << reduce(dp,dp + 10000, 0, max<int>);
+    return 0;
+}
